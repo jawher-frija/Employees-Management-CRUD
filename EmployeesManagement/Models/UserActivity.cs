2d74e2eb2d87813b1b5fc3e9543080699f35ff1b
Frija Jawher <73180293+jawher-frija@users.noreply.github.com>
2024-03-12 13:43:11 +0100

    Ajoutez des fichiers projet.

---
+++ EmployeesManagement/Models/UserActivity.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeesManagement.Models
+{
+    public class UserActivity
+    {
+        public string? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public String? ModifieById { get; set; }
+        public DateTime ModifieOn { get; set; }
+    }
+}
